324ca6fe5a937b6176a8686b0d6eab15fc11a35d
Spenser Leighton <spenser.leighton@gmail.com>
2018-08-28 17:22:20 -0600

    Seed database

--- db/migrations/20180828152158_initial.js
+++ db/migrations/20180828152158_initial.js
@@ -5,14 +5,18 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary();
       table.string('state_name');
       table.string('state_nickname');
-      table.string('state_capitol');
+      table.string('state_capital');
 
       table.timestamps(true, true)
     }),
 
     knex.schema.createTable('state_facts', function(table) {
       table.increments('id').primary();
-      table.string('dumb_laws');
+      table.string('dumb_laws_1');
+      table.string('dumb_laws_2');
+      table.string('dumb_laws_3');
+      table.string('dumb_laws_4');
+      table.string('dumb_laws_5');
       table.string('worst_foods');
       table.string('weird_facts');
       table.string('weird_attractions');
---
+++ db/seeds/dev/state_info.js
@@ -0,0 +1,52 @@
+const stateData = require('../../../data/nicknames-capitals.json')
+const stateFactsData = require('../../../data/compile-data.js')
+console.log(stateFactsData)
+
+const createStateInfo = (knex, state) => {
+  return knex('state_info').insert({
+    state_name: state.state,
+    state_nickname: state.nickname,
+    state_capital: state.capital
+  }, 'id')
+  .then(stateId => {
+    let factPromises = []
+
+    const stateShit = stateFactsData.filter(fact => fact.state === state.state)
+    stateShit.forEach(shit => {
+      factPromises.push(
+        createStateFacts(knex, {
+          state_id: stateId[0],
+          dumb_laws_1: shit.dumbLaws[0],
+          dumb_laws_2: shit.dumbLaws[1],
+          dumb_laws_3: shit.dumbLaws[2],
+          dumb_laws_4: shit.dumbLaws[3],
+          dumb_laws_5: shit.dumbLaws[4],
+          worst_foods: shit.food,
+          weird_attractions: shit.attraction,
+          weird_facts: shit.worstFact
+        })
+      )
+    });
+    return Promise.all(factPromises)
+  })
+};
+
+const createStateFacts = (knex, state) => {
+  return knex('state_facts').insert(state);
+};
+
+
+exports.seed = (knex, Promise) => {
+  return knex('state_facts').del()
+    .then(() => knex('state_info').del())
+    .then(() => {
+      let statePromises = [];
+
+      stateData.forEach(state => {
+        statePromises.push(createStateInfo(knex, state));
+      });
+
+      return Promise.all(statePromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
