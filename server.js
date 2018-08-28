ff50d1a2510dae7e4474bdba9a59a185b9268569
Spenser Leighton <spenser.leighton@gmail.com>
2018-08-28 17:58:55 -0600

    Add GET state_info method

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/api/v1/state_info', (req, res) => {
+  database('state_info').select()
+    .then((state_info) => {
+      res.status(200).json(state_info)
+    })
+    .catch((err) => {
+      res.status(500).json({ err })
+    })
+})
+
+
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'B.Y.O.B.'
