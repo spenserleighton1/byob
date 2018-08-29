c1204e64ff998ab13a2899852af4bea82e2175f3
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-08-29 16:37:08 -0600

    Lint server.js

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/v1/state_info', (req, res) => {
   database('state_info').select()
     .then((state_info) => {
-      res.status(200).json(state_info)
+      res.status(200).json(state_info);
     })
-    .catch((err) => {
-      res.status(500).json({ err })
-    })
-})
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/state_facts', (req, res) => {
   database('state_facts').select()
     .then((state_facts) => {
-      res.status(200).json(state_facts)
+      res.status(200).json(state_facts);
     })
-    .catch((err) => {
-      res.status(500).json({ err })
-    })
-})
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/state_info/:id', (req, res) => {
   database('state_info').where('id', req.params.id).select()
@@ -35,12 +35,12 @@ app.get('/api/v1/state_info/:id', (req, res) => {
         res.status(200).json(state);
       } else {
         res.status(404).json({
-          error: `Could not find a state with id ${req.params.id}`
+          erroror: `Could not find a state with id ${req.params.id}`
         });
       }
     })
-    .catch(err => {
-      res.status(500).json({ err })
+    .catch(error => {
+      res.status(500).json({ error });
     });
 });
 
@@ -55,8 +55,8 @@ app.get('/api/v1/state_facts/:id', (req, res) => {
         });
       }
     })
-    .catch(err => {
-      res.status(500).json({ err })
+    .catch(error => {
+      res.status(500).json({ error });
     });
 });
 
@@ -84,53 +84,75 @@ app.post('/api/v1/state_info', (req, res) => {
     if (!stateInfo[requiredParam]) {
       return res
         .status(422)
-        .send({ error: `Expected format: { state_name: <STRING>, state_nickname: <STRING>, state_capital: <STRING> }. You are missing a "${requiredParam}" property.`})
+        .send({ error: `Expected format: 
+        { state_name: <STRING>, 
+          state_nickname: <STRING>, 
+          state_capital: <STRING> }. 
+          You are missing a "${requiredParam}" property.`});
     }
   }
   database('state_info').insert(stateInfo, 'id')
     .then(state => {
-      res.status(201).json({ id: state[0] })
+      res.status(201).json({ id: state[0] });
     })
-    .catch(err => {
-      res.status(500).json({ err })
-    })
-})
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
 app.post('/api/v1/state_facts', (req, res) => {
   const stateFacts = req.body;
 
-  for (let requiredParam of ['dumb_laws_1', 'dumb_laws_2', 'dumb_laws_3', 'dumb_laws_4', 'dumb_laws_5','worst_foods', 'weird_facts', 'weird_attractions']) {
+  for (let requiredParam of [
+    'dumb_laws_1', 
+    'dumb_laws_2', 
+    'dumb_laws_3', 
+    'dumb_laws_4', 
+    'dumb_laws_5', 
+    'worst_foods', 
+    'weird_facts', 
+    'weird_attractions']) {
     if (!stateFacts[requiredParam]) {
       return res
         .status(422)
-        .send({ error: `Expected format: {dumb_laws_1: <STRING>, dumb_laws_2: <STRING>, dumb_laws_3: <STRING>, dumb_laws_4: <STRING>, dumb_laws_5: <STRING>, worst_foods: <STRING>, weird_facts: <STRING>, weird_attractions: <STRING>, state_id: <INTEGER>}. You are missing a "${requiredParam}" property.`})
+        .send({ error: `Expected format: 
+          {dumb_laws_1: <STRING>, 
+            dumb_laws_2: <STRING>, 
+            dumb_laws_3: <STRING>, 
+            dumb_laws_4: <STRING>, 
+            dumb_laws_5: <STRING>, 
+            worst_foods: <STRING>, 
+            weird_facts: <STRING>, 
+            weird_attractions: <STRING>, 
+            state_id: <INTEGER>}. 
+            You are missing a "${requiredParam}" property.`});
     }
   }
   database('state_facts').insert(stateFacts, 'id')
     .then(state => {
-      res.status(201).json({ id: state[0] })
+      res.status(201).json({ id: state[0] });
     })
-    .catch(err => {
-      res.status(500).json({ err })
-    })
-})
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
 app.put('/api/v1/state_info/:id', (req, res) => {
   const stateInfo = req.body;
 
   database('state_info').where('id', req.params.id).update(stateInfo, 'id')
-  .then(state => {
-    if (state.length) {
-      return res.status(201).json({ id: state[0] })
-    } else {
-      return res.status(404).json({
-        error: `Could not find a state with id ${req.params.id}`
-      });
-    }
-  })
-    .catch(err => {
-      res.status(500).json({ err })
+    .then(state => {
+      if (state.length) {
+        return res.status(201).json({ id: state[0] });
+      } else {
+        return res.status(404).json({
+          error: `Could not find a state with id ${req.params.id}`
+        });
+      }
     })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 });
 
 app.put('/api/v1/state_facts/:id', (req, res) => {
@@ -147,16 +169,16 @@ app.put('/api/v1/state_facts/:id', (req, res) => {
   database('state_facts').where('id', req.params.id).update(stateFacts, 'id')
     .then(state => {
       if (state.length) {
-        return res.status(201).json({ id: state[0] })
+        return res.status(201).json({ id: state[0] });
       } else {
         return res.status(404).json({
           error: `Could not find a state with id ${req.params.id}`
         });
       }
     })
-    .catch(err => {
-      res.status(500).json({ err })
-    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 });
 
 
@@ -168,17 +190,17 @@ app.delete('/api/v1/state_info/:id', (req, res) => {
         'id': req.params.id
       });
     });
-})
+});
 
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'B.Y.O.B.'
+app.locals.title = 'B.Y.O.B.';
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the party.')
+  res.send('Welcome to the party.');
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
-})
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
 
