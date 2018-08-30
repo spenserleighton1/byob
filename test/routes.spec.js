3615affbb286ce4ba0090555de4078561e0a76c5
Spenser Leighton <spenser.leighton@gmail.com>
2018-08-29 18:03:35 -0600

    Update node env in test file

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
@@ -6,7 +8,18 @@ const knex = require('../db/knex');
 
 chai.use(chaiHttp);
 
+
+function beforeEachTest() {
+    beforeEach(function(done) {
+    knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run())
+    .then(() => done())
+  })
+}
+
 describe('API endpoints', () => {
+  beforeEachTest()
   it('should pass this test', done => {
     chai.request(server)
       .get('/')
